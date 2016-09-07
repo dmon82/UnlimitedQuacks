a9a1f9b8e98d2640665184e07e4472d3a4a0f090
dmon82 <d.mon@gmx.net>
2016-09-07 10:54:54 +0200

    Can now limit the number of stacks of ash to be produced via config.

--- src/com/wurmonline/ulviirala/mods/AshProduce.java
+++ src/com/wurmonline/ulviirala/mods/AshProduce.java
@@ -2,6 +2,7 @@ package com.wurmonline.ulviirala.mods;
 
 import com.wurmonline.server.items.Item;
 import com.wurmonline.server.items.ItemFactory;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,10 +21,12 @@ import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
 /**
  *
- * Makes forges, smelters, and kilns produce ash while burning with a 1 in N chance per second.
+ * Makes forges, ovens, campfires, smelters, and kilns produce ash while burning
+ * with a 1 in N chance per second.
  */
 public class AshProduce implements WurmServerMod, PreInitable, Configurable {
     private int _AshChance = 60;
+    private int _AshStacks = 1;
     
     @Override
     public void preInit() {
@@ -39,19 +42,23 @@ public class AshProduce implements WurmServerMod, PreInitable, Configurable {
                                 "{ this.insertItem(com.wurmonline.server.items.ItemFactory.createItem(141, this.getQualityLevel(), null), true); }");
                     }*/ 
                     if (methodCall.getMethodName().equals("getTemplateId")) {
-                        ctMethod.insertAt(methodCall.getLineNumber() + 1, 
+                        ctMethod.insertAt(methodCall.getLineNumber(), 
                         "{ if (new java.util.Random().nextInt(" + String.valueOf(_AshChance) + ") == 0) {" +
                              "com.wurmonline.server.items.Item[] forgeItems = this.getItemsAsArray();"+
                              "boolean combinedAsh = false;"+
+                             "int ashStacks = 0;"+
                              "for (int iForge = 0; iForge < forgeItems.length; iForge++) {"+
-                             "     if (forgeItems[iForge].getTemplateId() == 141 && forgeItems[iForge].getWeightGrams() < 6400) {"+
-                             "          forgeItems[iForge].setWeight(forgeItems[iForge].getWeightGrams() + 100, true);"+
-                             "          combinedAsh = true;"+
-                             "          break;"+
+                             "     if (forgeItems[iForge].getTemplateId() == 141) {"+
+                             "          ashStacks++;" +
+                             "          if (forgeItems[iForge].getWeightGrams() < 6400" + (_AshStacks > 0 ? " && ashStacks <= " + _AshStacks : "") + ") {"+
+                             "              forgeItems[iForge].setWeight(forgeItems[iForge].getWeightGrams() + 100, true);"+
+                             "              combinedAsh = true;"+
+                             "              break;"+
+                             "          }"+
                              "     }"+
                              "}"+
 
-                             "if (!combinedAsh) {"+
+                             "if (!combinedAsh" + (_AshStacks > 0 ? " && ashStacks < " + _AshStacks : "") + ") {"+
                              "     this.insertItem(com.wurmonline.server.items.ItemFactory.createItem(141, this.getCurrentQualityLevel(), null), true);"+
                              "}"+
                         "} }");
@@ -66,7 +73,9 @@ public class AshProduce implements WurmServerMod, PreInitable, Configurable {
     @Override
     public void configure(Properties properties) {
         _AshChance = Math.max(1, Integer.valueOf(properties.getProperty("ashChance", String.valueOf(_AshChance))));
+        _AshStacks = Math.max(0, Math.min(100, Integer.valueOf(properties.getProperty("ashStacks", String.valueOf(_AshStacks)))));
         
-        Logger.getLogger(AshProduce.class.getName()).log(Level.INFO, String.format("Forges, smelters, and kilns will now produce ash with a 1 in %d chance per second.", _AshChance));
+        Logger.getLogger(AshProduce.class.getName()).log(Level.INFO, String.format("Fireplaces will now produce ash with a 1 in %d chance per second.", _AshChance));
+        Logger.getLogger(AshProduce.class.getName()).log(Level.INFO, String.format("Fireplaces will produce %s stacks of ash.", (_AshStacks == 0 ? "unlimited" : String.valueOf(_AshStacks))));
     }
 }
